08e7f17e59f0e8b69c885248ccd35e69f9b37b5f
IBRAHIM KHALIL <you@example.com>
2021-06-14 07:06:01 -0700

    first commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+#Comment
 from pathlib import Path,os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
